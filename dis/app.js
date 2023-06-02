5a8df97d39676d76b93713de20fc39fb3a0f9ced
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2023-06-03 01:50:03 +0300

    Add API setup and controller attachment to app.ts
    Add the necessary functions and configurations in the app.ts file to create APIs and activate them.
    
    Changes Made:
    - Imported required dependencies, including express, decorators/express, chatController, database, cors, and bodyParser.
    - Created an express app.
    - Set up middleware for enabling CORS and parsing JSON and URL-encoded bodies.
    - Initialized the database connection.
    - Set up the server to listen on the specified port.
    - Attached controllers, specifically the ChatController, to the app.
    - This implementation sets up the foundation for creating APIs, connecting to the database, and handling incoming requests using the express framework.

---
+++ dis/app.js
@@ -0,0 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const express_2 = require("@decorators/express");
+const chatController_1 = __importDefault(require("./controllers/chatController"));
+const database_1 = __importDefault(require("./database"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.use((0, cors_1.default)());
+app.use(body_parser_1.default.json()); // Add this line to parse JSON bodies
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.json()); // Middleware to parse JSON request body
+database_1.default
+    .initialize()
+    .then(() => {
+    console.log("db is connected");
+})
+    .catch((error) => {
+    console.log("🚀 ~ file: app.ts:14 ~ database.initialize ~ error:", error);
+});
+app.listen(port, () => {
+    console.log(`The server is running on http://localhost:${port}`);
+});
+(0, express_2.attachControllers)(app, [chatController_1.default]).then(() => {
+    console.log("Controller attached");
+});
---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { attachControllers } from "@decorators/express";
+import ChatController from "./controllers/chatController";
+import database from "./database";
+import cors from "cors";
+import bodyParser from "body-parser";
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(bodyParser.json()); // Add this line to parse JSON bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json()); // Middleware to parse JSON request body
+
+database
+  .initialize()
+  .then(() => {
+    console.log("db is connected");
+  })
+  .catch((error) => {
+    console.log("🚀 ~ file: app.ts:14 ~ database.initialize ~ error:", error);
+  });
+
+app.listen(port, () => {
+  console.log(`The server is running on http://localhost:${port}`);
+});
+
+attachControllers(app, [ChatController]).then(() => {
+  console.log("Controller attached");
+});
