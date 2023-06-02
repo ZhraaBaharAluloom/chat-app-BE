4632dfe4a9fe4ebe56346b410da20d492d18e54c
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2023-06-03 02:57:42 +0300

    fix port

--- dis/app.js
+++ dis/app.js
@@ -10,7 +10,7 @@ const database_1 = __importDefault(require("./database"));
 const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json()); // Add this line to parse JSON bodies
 app.use(body_parser_1.default.urlencoded({ extended: true }));
