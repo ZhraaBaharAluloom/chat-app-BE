630517fbb74abb167761b446e7daa9a8e11eeda8
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2023-06-16 11:35:51 +0300

    Connect ChatRoom Entity to DB

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,7 @@
 import { DataSource } from "typeorm";
 import Chat from "./entities/chat";
 import User from "./entities/user";
+import ChatRoom from "./entities/chatRoom";
 
 const database = new DataSource({
   username: process.env.USERNAME,
@@ -9,7 +10,7 @@ const database = new DataSource({
   host: "localhost",
   type: "postgres",
   port: 5432,
-  entities: [Chat, User],
+  entities: [Chat, User, ChatRoom],
   synchronize: true,
 });
 
